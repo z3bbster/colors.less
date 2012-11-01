843e3e75b321980c689e7566a972e3135a19a02b
Jimmy King <jimmy.king@meltmedia.com>
2012-11-01 10:55:53 -0700

    Add feature to demo for adjust params

--- js/site.js
+++ js/site.js
@@ -1,14 +1,42 @@
-window.onload = function() {
+$(function(){
+
   localStorage.clear();
+  
+	var getParams = function(){
+    var params = [];
+  	$('input.param').each(function(){
+    	var $this = $(this);
+    	params.push($this.val());
+  	})
+  	return params;
+	};
+	
+  var paramListener = function(){
+    var params = getParams();
+    less.modifyVars({
+      '@difference': params[0] + '%',
+      '@distance1': params[1],
+      '@distance2': params[2],
+      '@angle': params[3],
+      '@distance3': params[4],
+      '@distance4': params[5],
+      '@distance5': params[6],
+      '@distance6': params[7]
+    });
+  };
+  
+  $('input.param').on('keyup',paramListener);
+
 	picker = new Color.Picker({
 		color: "#FD00FF", // accepts rgba(), or #hex
 		display: true,
 		size: 200,
 		callback: function(rgba, state, type) {
-			var baseColor = Color.Space(rgba, "RGBA>W3");
+			baseColor = Color.Space(rgba, "RGBA>W3");
 			less.modifyVars({
         '@baseColor': baseColor
       });
+      console.log(baseColor);
 		}
 	});
-};
+});
