4bf1160d6f4d7d0014b526634432c73dba03159a
Jimmy King <jimmy.king@meltmedia.com>
2012-11-05 13:57:00 -0700

    Cleaned up JS & fixed issue in demo that caused changing one of the params to reset the baseColor and visa versa

--- js/site.js
+++ js/site.js
@@ -2,18 +2,16 @@ $(function(){
 
   localStorage.clear();
   
+  var baseColor;
+  
 	var getParams = function(){
-    var params = [];
+  	var params = [];
   	$('input.param').each(function(){
     	var $this = $(this);
     	params.push($this.val());
-  	})
-  	return params;
-	};
-	
-  var paramListener = function(){
-    var params = getParams();
-    less.modifyVars({
+  	})	  
+	  var lessVars = {
+      '@baseColor': baseColor,
       '@difference': params[0] + '%',
       '@distance1': params[1],
       '@distance2': params[2],
@@ -22,20 +20,25 @@ $(function(){
       '@distance4': params[5],
       '@distance5': params[6],
       '@distance6': params[7]
-    });
+    };
+    return lessVars;
+	};
+	
+  var paramListener = function(){
+    lessVars = getParams();
+    less.modifyVars(lessVars);
   };
   
   $('input.param').on('keyup',paramListener);
 
-	picker = new Color.Picker({
-		color: "#FD00FF", // accepts rgba(), or #hex
-		display: true,
-		size: 200,
-		callback: function(rgba, state, type) {
-			baseColor = Color.Space(rgba, "RGBA>W3");
-			less.modifyVars({
-        '@baseColor': baseColor
-      });
-		}
-	});
+  picker = new Color.Picker({
+    color: "#FD00FF", // accepts rgba(), or #hex
+    display: true,
+    size: 200,
+    callback: function(rgba, state, type) {
+      baseColor = Color.Space(rgba, "RGBA>W3");
+      lessVars = getParams();
+      less.modifyVars(lessVars);
+    }
+  });
 });
